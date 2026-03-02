0ac0a1d6e3f0fb54dbd0d21e282bc14ba2bf8df9
byissa <codedbyissa@gmail.com>
2026-03-02 14:01:48 -0300

    update .env

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import Server from "./server/index";
 import routes from "./server/routes";
 import { start } from "./bot";
 
-const port = 3000; 
+const port = Number(process.env.PORT) || 3000;
 export default new Server().router(routes).listen(port);
 start();
